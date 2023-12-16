76c1bcaf16b86678b71fdf862b79df10b6f25807
suraj-bv <surajbv5566@gmail.com>
2023-12-16 12:53:54 +0530

    swap

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a=5,b=10,temp;
+cout<<"Before swaping"<<endl;
+cout<<"a="<<a<<" b="<<b<<endl;
+temp=a;
+a=b;
+b=temp;
+cout<<"\nAfter swaping"<<endl;
+cout<<"a="<<a<<" b="<<b<<endl;
+return 0;
+}
