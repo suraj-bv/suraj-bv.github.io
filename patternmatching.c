b34b4fdee4ea7fefee429db5321c945cf7b23fb2
suraj-bv <surajbv5566@gmail.com>
2023-12-16 11:29:46 +0530

    pattern

---
+++ patternmatching.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<string.h>
+void readdata(char str[100],char pat[50],char rep[50])
+{
+printf("Enter the main string:\n");
+gets(str);
+printf("Enter the pattern string:\n);
+gets(pat);
+printf("Enter the replace string:\n);
+gets(rep);
+}
+void patternmatching(char str[100],char pat[50],char rep[50])
+{
+int i=0,j=0,c=0,m=0,k=0,flag=0;
+char ans[100];
+while(str[c]!='\0')
+{
+if(str[m]==pat[i])
+{
+i++,m++;
+flag=1;
+if(pat[i]=='\0')
+{
+for(k=0;rep[k]!='\0';k++,j++)
+ans[j]=rep[k];
+i=0;c=m;
+}
+}
+else{
+ans[j]=str[c];
+j++;c++;
+m=c,i=0;
+}
+}
+if(flag==0)
+printf("Pattern does'nt match:");
+else{
+ans[j]='\0';
+printf("The resultant string is %s",ans);
+}
+}
+int main()
+{
+char str[100],rep[50],pat[50];
+int choice;
+printf("Enter your choice:\n");
+printf("Enter 1 to read a string:\n");
+printf("Enter 2 to print the resultant string:\n");
+printf("Enter 
